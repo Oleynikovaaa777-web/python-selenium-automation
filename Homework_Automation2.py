5293395bf7210a0b38918c4fa34c500564e91025
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-05 16:23:06 -0700

    Add Homework_Automation2.py

---
+++ Homework_Automation2.py
@@ -0,0 +1,28 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome()
+
+# open the url
+driver.get('https://www.amazon.com//')
+
+cart = driver.find_element(By.XPATH,"//a[@id='nav-cart']")
+cart.click()
+
+
+empty_cart= driver.find_element(By.XPATH,"//div[h2]")
+
+
+
+
+
+# assert 'Your Shopping Cart is empty' in empty_cart.text, \
+#     f'Expected \Your Shopping Cart is empty', but got {empty_cart.text}'
+
+
+
+
+driver.quit()
+
