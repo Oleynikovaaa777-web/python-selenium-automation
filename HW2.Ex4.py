7f22f1e09b87812b01bc69bf44ff7bdaa6e37191
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-05 16:31:46 -0700

    Add HW2.Ex.4.py

---
+++ HW2.Ex4.py
@@ -0,0 +1,3 @@
+# Create your own test case to add any product you want into the cart, and make sure it’s there (check for the number of items in the cart OR open the cart and verify it’s there, up to you!)
+# * - Not Required
+
