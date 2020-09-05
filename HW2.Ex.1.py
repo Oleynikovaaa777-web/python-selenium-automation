c79319dce3a3c1e534779bd3df96cff05d06ead7
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-05 16:25:31 -0700

    Add HW2.Ex.1.py

---
+++ HW2.Ex.1.py
@@ -0,0 +1,18 @@
+
+$x("//a[@class='a-link-nav-icon']")
+
+$x("//input[@class='a-input-text a-span12 auth-autofocus auth-required-field auth-contact-verification-request-info']")
+
+$x("//input[@class='a-input-text a-span12 auth-required-field auth-require-fields-match auth-require-email-validaton auth-require-reverify-on-change auth-contact-verification-request-info']")
+
+$x("//input[@class='a-input-text a-form-normal a-span12 auth-required-field auth-require-fields-match auth-require-password-validation']")
+
+$x("//input[@class='a-input-text a-form-normal a-span12 auth-required-field auth-require-fields-match']")
+
+$x("//input[@id='continue']")
+
+$x("//a[text()='Conditions of Use']")
+
+$x("//a[text()='Privacy Notice']")
+
+$x("//a[@class='a-link-emphasis']")
