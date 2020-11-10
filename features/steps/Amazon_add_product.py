6757e7804b768a86ad3722fc54a99ba9485da224
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-11-09 17:00:25 -0800

    Add HW2.Ex.4.py

--- features/steps/Amazon_add_product.py
+++ features/steps/Amazon_add_product.py
@@ -1,48 +1,48 @@
-from selenium.webdriver.common.by import By
-from behave import given, when, then
-from time import sleep
-
-
-CART_ICON = (By.ID, 'nav-cart')
-EMPTY = (By.CSS_SELECTOR, '.sc-your-amazon-cart-is-empty h2')
-SEARCH_FIELD = (By.ID, 'twotabsearchtextbox')
-SEARCH_BUTTON = (By.ID, 'nav-search-submit-text')
-LEGO_HARRY = (By.CSS_SELECTOR, '#anonCarousel1 .a-section .a-size-mini .a-link-normal')
-ADD_TO_CART = (By.ID, 'add-to-cart-button')
-
-
-
-
-
-
-
-@given('Open Amazon page')
-def open_amazon(context):
-    context.driver.get('https://www.amazon.com/')
-
-
-@when('Input {search_word} into search field')
-def input_search(context, search_word):
-    search = context.driver.find_element(*SEARCH_FIELD)
-    search.clear()
-    search.send_keys(search_word)
-    sleep(4)
-
-
-@when('Click on search icon')
-def click_search_icon(context):
-    context.driver.find_element(*SEARCH_BUTTON).click()
-    sleep(1)
-
-@when('Choose a first item in result list')
-def click_first_item(context):
-    context.driver.find_element(*LEGO_HARRY).click()
-    sleep(1)
-
-@then ('Add it to cart')
-def add_to_cart(context):
-    context.driver.find_element(*ADD_TO_CART).click()
-    sleep(1)
+# from selenium.webdriver.common.by import By
+# from behave import given, when, then
+# from time import sleep
+#
+#
+# CART_ICON = (By.ID, 'nav-cart')
+# EMPTY = (By.CSS_SELECTOR, '.sc-your-amazon-cart-is-empty h2')
+# SEARCH_FIELD = (By.ID, 'twotabsearchtextbox')
+# SEARCH_BUTTON = (By.ID, 'nav-search-submit-text')
+# LEGO_HARRY = (By.CSS_SELECTOR, '#anonCarousel1 .a-section .a-size-mini .a-link-normal')
+# ADD_TO_CART = (By.ID, 'add-to-cart-button')
+#
+#
+#
+#
+#
+#
+#
+# @given('Open Amazon page')
+# def open_amazon(context):
+#     context.driver.get('https://www.amazon.com/')
+#
+#
+# @when('Input {search_word} into search field')
+# def input_search(context, search_word):
+#     search = context.driver.find_element(*SEARCH_FIELD)
+#     search.clear()
+#     search.send_keys(search_word)
+#     sleep(4)
+#
+#
+# @when('Click on search icon')
+# def click_search_icon(context):
+#     context.driver.find_element(*SEARCH_BUTTON).click()
+#     sleep(1)
+#
+# @when('Choose a first item in result list')
+# def click_first_item(context):
+#     context.driver.find_element(*LEGO_HARRY).click()
+#     sleep(1)
+#
+# @then ('Add it to cart')
+# def add_to_cart(context):
+#     context.driver.find_element(*ADD_TO_CART).click()
+#     sleep(1)
 
 
 
---
+++ features/tests/Amazon.homework.feature
@@ -0,0 +1,11 @@
+  Feature: Test Scenarios for Cart functionality
+
+  Scenario: User can add to Cart
+    Given Open Amazon page
+    When Click on Cart icon
+    Then Verify that Cart is empty
+    Then Input lego into search field
+    Then Click on search icon
+    And Choose a first item in result list
+    And Add it to cart
+    Then Verify that result contains 1
